4b0c8e159650163b0980a360c3164f5c340dbcdf
david2torres <dadyotorres1403@gmail.com>
2019-07-30 21:38:07 -0500

    version1

---
+++ target/classes/import.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO restaurantes (nombre_comercial,razon_social,tipo_restaurante) values ( 'Repapitas','Patacon Sason',1);
+INSERT INTO restaurantes (nombre_comercial,razon_social,tipo_restaurante) values ('RutaSol','Montaña',2);
+INSERT INTO restaurantes (nombre_comercial,razon_social,tipo_restaurante) values ('trucha','Pez y Trucha', 3);
+INSERT INTO restaurantes (nombre_comercial,razon_social,tipo_restaurante) values ('Salsa','Salsa y Sabor',4);
+
+INSERT INTO menus (ingredientes, nombre_menu, precio, tipo_menu) values ('salsa','costilas ss',12500, 1);
+INSERT INTO menus (ingredientes, nombre_menu, precio, tipo_menu) values ('arroz','arros atollado',13200,2);
+INSERT INTO menus (ingredientes, nombre_menu, precio, tipo_menu) values ('lomo','cerdo',7200,3);
+INSERT INTO menus (ingredientes, nombre_menu, precio, tipo_menu) values ('carnes','res',16000,4);
